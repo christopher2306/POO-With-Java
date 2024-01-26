807365fec50a6da71087d903cb243967d726a287
Christopher Antony <christophercarreira2306@gmail.com>
2024-01-26 00:12:53 -0300

    Implementando funções de saque e transferência

--- src/contas/Conta.java
+++ src/contas/Conta.java
@@ -13,8 +13,38 @@ public class Conta {
 	
 //Um método "void" é aquele que executa ação sem a necessidade de retornar algo.	
 //O "this" informa o saldo é da classe.
+	
+	
 	public void deposita(double valor){
 		this.saldo = this.saldo + valor;
 	}
 	
+	
+//boolean retorna true ou false	
+	public boolean saca(double valor) {
+		
+		if(this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			System.out.println("Saque efetuado!");
+			return true;
+		} else {
+			System.out.println("Erro na transação de saque!");
+			return false;
+		}
+		
+	}
+	
+	
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			System.out.println("Transferência realizada com sucesso.");
+			return true;
+		} else {
+			System.out.println("Transferência negada.");
+			return false;
+		}
+	}
 }
--- src/contas/TestaMetodo.java
+++ src/contas/TestaMetodo.java
@@ -7,11 +7,28 @@ public class TestaMetodo {
 		
 		Conta contaChris = new Conta();
 		
+		System.out.println("-----------DEPOSITA-----------");
 		contaChris.saldo = 150;
 		System.out.println("O seu saldo é: " + contaChris.saldo);
 		
 		contaChris.deposita(400);
 		System.out.println("O novo saldo é: " + contaChris.saldo);
+		
+		System.out.println("\n\n\n-----------SACA-----------");
+		contaChris.saca(600);
+		System.out.println("O novo saldo é: " + contaChris.saldo);
+		
+		System.out.println("\n\n\n-----------TRANSFERE-----------");
+		Conta contaMaria = new Conta();
+		contaMaria.saldo = 400;
+		
+		System.out.println("Saldo Maria: " + contaMaria.saldo);	
+		System.out.println("Saldo Chris: " + contaChris.saldo);
+		
+		contaMaria.transfere(200, contaChris);
+		
+		System.out.println("Saldo Maria: " + contaMaria.saldo);	
+		System.out.println("Saldo Chris: " + contaChris.saldo);	
 	}
 	
 }
